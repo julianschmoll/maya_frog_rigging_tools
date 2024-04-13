cd53cbe7a548d28cbef90b0765a1181adeb46d21
Julian Schmoll <dev@julianschmoll.de>
2024-04-13 02:46:51 +0200

    remove ctl dict

--- maya_frog_rigging_tools/control.py
+++ maya_frog_rigging_tools/control.py
@@ -1,18 +1,18 @@
 from ._control import utils
 import os
+from pathlib import Path
+import logging
 
+# Get the current working directory
+cwd = Path.cwd()
+parent_dir = os.path.dirname(cwd)
+json_dir = os.path.join(parent_dir, "resources", "controls")
 
-json_dir = os.path.join(
-    os.path.abspath(os.curdir), "resources", "controls"
-)
-
-
-ctl_dict = {
-    "gear": os.path.join(json_dir, "gear.json")
-}
+LOGGER = logging.getLogger("Rig Controls")
 
 
 def create(ctl_type, name="ctl", size=1):
-    json_path = ctl_dict.get(ctl_type)
+    LOGGER.info(json_dir)
+    json_path = os.path.join(json_dir, f"{ctl_type}.json")
     if json_path:
         return utils.create_ctl_from_json(json_path, name, ctl_size=size)
